75462ee8f42f0100ee61e1e644c2a69780d54c89
Doyage <pcstt@naver.com>
2025-07-25 21:46:14 +0900

    feat(backend): add PrismaModule import to GlobalStats and KeyValue modules

--- apps/backend/src/global-stats/global-stats.module.ts
+++ apps/backend/src/global-stats/global-stats.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { GlobalStatsController } from './global-stats.controller';
 import { GlobalStatsService } from './global-stats.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GlobalStatsController],
   providers: [GlobalStatsService],
 })
--- apps/backend/src/key-value/key-value.module.ts
+++ apps/backend/src/key-value/key-value.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { KeyValueController } from './key-value.controller';
 import { KeyValueService } from './key-value.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [KeyValueController],
   providers: [KeyValueService],
 })
