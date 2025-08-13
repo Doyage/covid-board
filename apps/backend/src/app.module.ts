9534279785f32f8b9169ffdeacf698619b7a7e92
Doyage <pcstt@naver.com>
2025-08-14 00:55:30 +0900

    refactor(backend): remove unused AppController, AppService, and CountiresController; update CountriesModule imports

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CountriesModule } from './countries/countries.module';
 import { GlobalStatsModule } from './global-stats/global-stats.module';
 import { KeyValueModule } from './key-value/key-value.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { PrismaService } from './prisma/prisma.service';
-import { CountiresController } from './countires/countires.controller';
 
 @Module({
   imports: [GlobalStatsModule, PrismaModule, KeyValueModule, CountriesModule],
-  controllers: [AppController, CountiresController],
-  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- apps/backend/src/countries/countries.module.ts
+++ apps/backend/src/countries/countries.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { CountriesController } from './countries.controller';
 import { CountriesService } from './countries.service';
 
 @Module({
-  providers: [CountriesService],
+  imports: [PrismaModule],
   controllers: [CountriesController],
+  providers: [CountriesService],
 })
 export class CountriesModule {}
